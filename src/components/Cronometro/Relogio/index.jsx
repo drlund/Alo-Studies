9c0005e1bdf814ce29c7617ae2d1e0d394ef169d
Paulo César Souza Pereira <drlund@hotmail.com>
2022-12-19 14:10:06 -0300

    Update index.jsx

--- src/components/Cronometro/Relogio/index.jsx
+++ src/components/Cronometro/Relogio/index.jsx
@@ -1,17 +1,13 @@
-import Botao from "../Botao";
-import Relogio from "./Relogio";
-import style from './Cronometro.module.scss';
+import style from './Relogio.module.scss';
 
-export default function Cronometro() {
+export default function Relogio() {
   return (
-    <div className={style.cronometro}>
-      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
-      <div className={style.relogioWrapper}>
-        <Relogio />
-      </div>
-      <Botao>
-        Começar!
-      </Botao>
-    </div>
+    <>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+    </>
   )
 }
