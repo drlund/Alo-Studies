725326bcbdc43c9b3cc4d2cd9d27d0d339b2924f
Paulo César Souza Pereira <drlund@hotmail.com>
2022-12-19 10:47:14 -0300

    Update index.tsx

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -32,6 +32,7 @@ class Formulario extends React.Component {
                         required
                     />
                     <Botao />
+                    texto="Adicionar"
                 </div>
             </form>
         )
